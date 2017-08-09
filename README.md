97cda59bb806661b58ea4da8bbd57ba517586e35
swimIan <swimIan@gmail.com>
2017-08-09 10:47:49 -0700

    Update files

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# SwitchSensorMashStep
+# SensorSwitchMashStep Plugin for CraftBeerPi 3.0
+
+This is a very simple plugin that adds a second temperature sensor to the MashStep.  This step set the kettle temperature based on the sensor previously assigned to the kettle and turn on a secondary actor (ex. pump, etc.).  Then the step timer will not start until the secondary sensor value is equal to the kettle sensor value.  This is would be useful for a HERMES or similiar system in which you are not directly heating your mash or you are not directly heating your mash with a high-powered heat source.
+
+## Usage
+1.  Download the Sensor Switch MashStep plugin from within CraftBeerPi 3.0, and then reboot your Raspberry Pi.
+2.  Configure a new brewing step, and select SwitchSensorMashStep as the type.
+3.  Enter values for the following settings:
+    1.  **temp**: Target Temperature of Mash Step.
+    2.  **kettle**: Kettle in which the mashing takes place.
+    3.  **timer**: Timer is started when the target temperature is reached.
+    4.  **sensor**: Selectable secondary sensor
+    5.  **pump**: Pump, et.
+    4.  Click **Update** to save your configuration.
---
+++ __init__.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+import time
+
+
+from modules.core.props import Property, StepProperty
+from modules.core.step import StepBase
+from modules import cbpi
+
+@cbpi.step
+class SwitchSensorMashStep(StepBase):
+    '''
+    Just put the decorator @cbpi.step on top of a method
+    '''
+    # Properties
+    temp = Property.Number("Temperature", configurable=True, description="Target Temperature of Mash Step")
+    kettle = StepProperty.Kettle("Kettle", description="Kettle in which the mashing takes place")
+    timer = Property.Number("Timer in Minutes", configurable=True, description="Timer is started when the target temperature is reached")
+    sensor = StepProperty.Sensor("Sensor", description="Selectable secondary sensor")
+    pump = StepProperty.Actor("Pump, etc.")
+
+    def init(self):
+        '''
+        Initialize Step. This method is called once at the beginning of the step
+        :return:
+        '''
+        # set target tep
+        self.set_target_temp(self.temp, self.kettle)
+        self.actor_on(int(self.pump))
+
+    @cbpi.action("Start Timer Now")
+    def start(self):
+        '''
+        Custom Action which can be execute form the brewing dashboard.
+        All method with decorator @cbpi.action("YOUR CUSTOM NAME") will be available in the user interface
+        :return:
+        '''
+        if self.is_timer_finished() is None:
+            self.start_timer(int(self.timer) * 60)
+
+    def reset(self):
+        self.stop_timer()
+        self.set_target_temp(self.temp, self.kettle)
+
+    def finish(self):
+        self.set_target_temp(0, self.kettle)
+        self.actor_off(int(self.pump))
+
+    def execute(self):
+        '''
+        This method is execute in an interval
+        :return:
+        '''
+
+        # Check if Target Temp is reached
+        if cbpi.get_sensor_value(int(self.sensor)) >= float(self.temp):
+            if self.is_timer_finished() is None:
+                self.start_timer(int(self.timer) * 60)
+
+        # Check if timer finished and go to next step
+        if self.is_timer_finished() == True:
+            self.notify("Mash Step Completed!", "Starting the next step", timeout=None)
+            self.next()
